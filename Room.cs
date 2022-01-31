7fb1b46c3e16bf7f5c0f5a5bc2d395d68ae8a337
Viktor Izsóf <vizsof@gmail.com>
2022-01-31 14:24:30 +0000

    Base Room and RoomManager layout

---
+++ Room.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2ndtrimtest
+{
+    /// <summary>
+    /// Basic single unit used in Procedural Generation
+    /// </summary>
+    class Room
+    {
+        //Room properties
+        public float width;
+        public float height;
+
+        //drawing start pos
+        public int x, y;
+
+        public string name = "X";
+
+        //imagine a lot more content
+
+
+    }
+}
---
+++ RoomManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2ndtrimtest
+{
+    class RoomManager
+    {
+        public Room startingRoom;
+        public Room endRoom;
+
+
+
+
+    }
+}
