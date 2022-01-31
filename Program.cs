98a5153897981d95541dc81e41d9bb42e84c5a1c
Viktor Izsóf <vizsof@gmail.com>
2022-01-31 15:05:10 +0000

    first room generation iteration

--- Program.cs
+++ Program.cs
@@ -4,10 +4,15 @@ namespace _2ndtrimtest
 {
     class Program
     {
+        static RoomManager roomManager;
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             //master test 2 now doing the stream to show this off branching test
+            roomManager = new RoomManager();
+            //roomManager.RoomInit();
         }
 
         public void SpecialFunctionality()
